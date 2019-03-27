fff4bd5e9475a2c1fceedfde0033d09ac4974c57
kingsquallo <doanchitaidn@gmail.com>
2019-03-27 23:39:00 +0700

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATABASE_URL = parse_url('postgres://dsbebylzljcizo:24d20dc6d717f9b6a5eb31e75a10954a343d7cf780811831a33a803565db7d86@ec2-50-17-231-192.compute-1.amazo
+naws.com:5432/d5uj8iurconq13');
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -58,16 +60,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
