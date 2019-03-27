ba0089fadc88c1b764c05577bafd61f3426bf72c
kingsquallo <doanchitaidn@gmail.com>
2019-03-27 22:07:36 +0700

    remove debugbar

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
     ],
