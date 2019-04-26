a801434b1d122c29d3024cd573cfed2e95e60a5e
kingsquallo <doanchitaidn@gmail.com>
2019-04-27 02:32:54 +0700

    Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('APP_ENV') === 'production') {
-            \URL::forceSchema('https');
+            \URL::forceScheme('https');
         }
         Schema::defaultStringLength(191);
         $views = [
